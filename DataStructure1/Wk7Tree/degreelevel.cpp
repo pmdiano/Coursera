cfe7378b2b85935266e24fd54bba1e0a39bb6e22
pmdiano <pmdiano@gmail.com>
2015-10-15 21:50:42 -0700

    DS1, week 7, tree and forest,  problem 1
    https://www.coursera.org/learn/shuju-jiegou-suanfa/programming/L0pOe/bia
    n-cheng-zuo-ye-shu-3ti

--- DataStructure1/Wk7Tree/degreelevel.cpp
+++ DataStructure1/Wk7Tree/degreelevel.cpp
@@ -1,7 +1,17 @@
+//+-----------------------------------------------------------------------------
+//
+//  File:       degreelevel.h
+//
+//  Synopsis:   Input degree-width representation of a forest, output its
+//              root last traversal.
+//              https://www.coursera.org/learn/shuju-jiegou-suanfa/programming
+//               /L0pOe/bian-cheng-zuo-ye-shu-3ti
+//
+//  Author:     Qirong Ma, 10/15/2015
+//
+//------------------------------------------------------------------------------
 #include <cstdio>
 #include <iostream>
-#include <string>
-#include <sstream>
 #include "../../include/tree.h"
 using namespace std;
 
@@ -15,26 +25,52 @@ struct elem{
 int main() {
 	int n, d;
 	char c;
-	string line;
+	TreeNode<char> *root, *current = nullptr;
 	Tree<char> tree;
-	TreeNode<char> *root, *current;
+	elem father;
 	queue<elem> q;
 
-	tree.createRoot(0);
+	(cin >> n).get();
+	cin >> c >> d;
+
+	tree.createRoot(c);
 	root = tree.getRoot();
-	current = root;
+	if (d) q.push(elem(root, d));
 
-	cin >> n;
-	while (n--) {
-		cin >> line;
-		istringstream is(line);
+	while (true) {
+		while (!q.empty() || current) {
+			cin >> c >> d;
+			TreeNode<char>* node = new TreeNode<char>(c);
+			if (d) q.push(elem(node, d));
+			if (!current) {
+				father = q.front();
+				q.pop();
+				father.node->setChild(node);
+			}
+			else {
+				current->setSibling(node);
+			}
 
-		while (is >> c >> d) {
-			current->setValue(c);
-			if (d)
-				q.push(elem(current, d));
-
+			if (--father.degree)
+				current = node;
+			else
+				current = nullptr;
 		}
+
+		if (--n) {
+			cin.get();
+			cin >> c >> d;
+			TreeNode<char>* node = new TreeNode<char>(c);
+			if (d) q.push(elem(node, d));
+			root->setSibling(node);
+			root = node;
+		}
+		else
+			break;
 	}
+
+	tree.rootLastTraverse([](char c){printf("%c ", c);});
+	printf("\n");
+
 	return 0;
 }
